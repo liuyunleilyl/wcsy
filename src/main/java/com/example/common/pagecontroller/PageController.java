8b2eb5bad5442051de4d1fc5598728b8701594bf
liuly <lyl18353623709>
2020-06-12 14:49:04 +0800

    2020.06.12

---
+++ src/main/java/com/example/common/pagecontroller/PageController.java
@@ -0,0 +1,19 @@
+package com.example.common.pagecontroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: liuyl
+ * @Date: 2020/6/12 12:33
+ * @Version: 1.0
+ * @Description:
+ */
+@RestController
+public class PageController {
+    /*@RequestMapping(value = "/login.jsp", method = RequestMethod.GET)
+    public String index() {
+        return "login.jsp";
+    }*/
+}
